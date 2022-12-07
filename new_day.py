408e1ae3ae0d2b6ba01d00ee0c2e0b9e3d45515a
Aman Jaiman <amanjaiman@outlook.com>
2022-12-07 12:11:44 -0500

    Update new_day.py

--- new_day.py
+++ new_day.py
@@ -3,20 +3,6 @@ import sys
 
 def main():
     n = len(sys.argv)
-    if n != 3:
-        return "Usage: d3.py <input_file> <p1 | p2>"
-    
-    input_file = sys.argv[1]
-    part = sys.argv[2]
-    if part == "p1":
-        return d3_p1(input_file)
-    elif part == "p2":
-        return d3_p2(input_file)
-    else:
-        return "Invalid problem part. Must be either p1 or p2"
-
-def main():
-    n = len(sys.argv)
     if n != 2:
         print("Usage: new_day.py <day_number>")
         return
@@ -35,6 +21,8 @@ def main():
     with open(prefix+".sample.input", "w+") as f:
         f.write("")
     
+    # wanted to download the input as well but you cannot without logging in
+
     return
 
 if __name__ == "__main__":
